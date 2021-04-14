bda9d23b296046def189e12a0713f5c6e7d6bbd1
Marco Grimaldo <marco.a.grimaldo98@gmil.com>
2021-04-14 17:13:57 -0500

    Creando el contador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    M.AutoInit();
+  });
+
+  const [counter,setCounter] = useState(0);
+  const [salto,setSalto] = useState(1);
+
+  const add = () => {
+    setCounter(counter + salto);
+  };
+
+  const substract = () => {
+    setCounter(counter - salto);
+  };
+
+  const addSalto = () => {
+    setSalto(salto + 1);
+  };
+
+  const substractSalto = () => {
+    setSalto(salto - 1);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <section className="row">
+        <div className="col s12 m6 offset-m3">
+          <div className="card-panel center" style={{backgroundColor: "#126e82"}}>
+            <h1 className="white-text">
+              {counter}
+            </h1>
+            <button
+              className="indigo waves-effect waves-light btn"
+              onClick={add}
+              style={{width: "33%"}}
+            >
+              <i className="material-icons">add</i>
+            </button>
+
+            <button
+              className="indigo waves-effect waves-light btn"
+              onClick={substract}
+              style={{width: "33%"}}
+            >
+              <i className="material-icons">remove</i>
+            </button>
+
+            <button
+              className="indigo waves-effect waves-light btn"
+              onClick={() => setCounter(0)}
+              style={{width: "66%",marginTop:"1rem"}}
+            >
+              Reset
+            </button>
+            
+          </div>
+        </div>
+
+        <div className="col s12 m6 offset-m3 center">
+          
+          <h6>Salto actual: </h6>
+          
+        </div>
+        <div className="col s12 m6 offset-m3 center">
+          <div className="col s4">
+            <button 
+              className=" indigo darken-2 btn-floating btn-large waves-effect waves-light"
+              onClick={addSalto}
+            >
+              <i className="material-icons">add</i>
+            </button>
+          </div>
+
+          <div className="col s4">
+            <h5>{salto}</h5>
+          </div>
+
+          <div className="col s4">
+            <button 
+              className=" indigo darken-2 btn-floating btn-large waves-effect waves-light"
+              onClick={substractSalto}
+            >
+              <i className="material-icons">remove</i>
+            </button>
+          </div>
+
+          <div className="col s12">
+            <button
+              className="indigo darken-2 waves-effect waves-light btn"
+              onClick={() => setSalto(1)}
+              style={{width: "33%",marginTop:"1rem"}}
+            >
+              Reset Salto
+            </button>
+          </div>
+
+        </div>
+      </section>
+      
+    </main>
   );
 }
 
